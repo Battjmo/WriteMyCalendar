97f330cb3132f2f91ce3ef37760ebc0980b57dc5
Nick Battjes <nhbattjes@gmail.com>
2024-06-03 14:57:28 -0700

    take a picture

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,15 +8,24 @@ import {
   Button,
 } from "react-native";
 import { useState } from "react";
-import { CameraView, useCameraPermissions } from "expo-camera";
+import {
+  CameraView,
+  useCameraPermissions,
+  Camera,
+  CameraViewRef,
+} from "expo-camera";
+import { CameraType } from "react-native-camera";
 // import { HelloWave } from "@/components/HelloWave";
 // import ParallaxScrollView from "@/components/ParallaxScrollView";
 // import { ThemedText } from "@/components/ThemedText";
 //
 
 export default function HomeScreen() {
-  const [facing, setFacing] = useState("back");
+  const [facing, setFacing] = useState<CameraType>("back");
   const [permission, requestPermission] = useCameraPermissions();
+  const [isCameraReady, setIsCameraReady] = useState(false);
+
+  let camera: CameraView | null = null;
 
   if (!permission) {
     // Camera permissions are still loading.
@@ -36,15 +45,41 @@ export default function HomeScreen() {
   }
 
   function toggleCameraFacing() {
+    console.log("toggling");
     setFacing((current) => (current === "back" ? "front" : "back"));
+  }
+
+  function setCameraReady() {
+    setIsCameraReady(true);
+  }
+
+  async function takeTheDamnPicture() {
+    const photo = await camera?.takePictureAsync();
+    console.log(photo);
+  }
+
+  function TakePictureButton() {
+    return (
+      <TouchableOpacity style={styles.button} onPress={takeTheDamnPicture}>
+        <Text style={styles.text}>Take Picture</Text>
+      </TouchableOpacity>
+    );
   }
 
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
+      <CameraView
+        style={styles.camera}
+        onCameraReady={setCameraReady}
+        ref={(ref) => (camera = ref)}
+        facing={facing}
+      >
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
             <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={takeTheDamnPicture}>
+            <Text style={styles.text}>Take Photo</Text>
           </TouchableOpacity>
         </View>
       </CameraView>
